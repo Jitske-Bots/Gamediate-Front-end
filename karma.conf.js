5f2704b0cebacef8e5e4e9f7a1b1d6716cc2710e
Jitske-Bots <58811242+Jitske-Bots@users.noreply.github.com>
2022-01-13 12:32:52 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-sonarqube-unit-reporter')
     ],
     client: {
       jasmine: {
@@ -34,10 +35,6 @@ module.exports = function (config) {
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
     },
-    
-    plugins: [
-      require('karma-sonarqube-unit-reporter')
-    ],
     sonarQubeUnitReporter: {
       sonarQubeVersion: 'LATEST',
       outputFile: 'reports/ut_report.xml',
